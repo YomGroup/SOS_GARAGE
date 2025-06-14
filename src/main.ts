9bb6d745e3c9954c37b3a7d3b5cc9c18007245aa
elouafim <mohammedelouafi02@icloud.com>
2025-06-14 23:44:21 +0100

    add keyckoak configuration

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,35 @@ import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app.routes'; 
 
 (async () => {
   const keycloakService = new KeycloakService();
 
   await keycloakService.init({
     config: {
-      url: 'http://localhost:8080/auth',
+      url: 'http://localhost:8080',
       realm: 'sos-garage',                      
-      clientId: 'sosmongarage', 
+      clientId: 'sosmongarage',
     },
     initOptions: {
       onLoad: 'login-required',
       checkLoginIframe: false,
       pkceMethod: 'S256',
+      redirectUri: 'http://localhost:4200/admin/dashboard',
     },
     enableBearerInterceptor: true,
   });
+  
 
   await bootstrapApplication(AppComponent, {
     providers: [
       provideHttpClient(),
-      importProvidersFrom(KeycloakAngularModule),
+      importProvidersFrom(
+        KeycloakAngularModule,
+        RouterModule.forRoot(routes)  // <-- Ici, on ajoute RouterModule
+      ),
       { provide: KeycloakService, useValue: keycloakService },
     ],
   });
