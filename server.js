ce682d2b1af915237e8cb16559f126e684e66187
elouafim <mohammedelouafi02@icloud.com>
2025-06-21 17:41:27 +0100

    add deploy configuration

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const appName = 'SOS_GARAGE'; // Assurez-vous que c'est le bon nom de projet
+
+// Servir les fichiers statiques de l'application Angular
+app.use(express.static(path.join(__dirname, 'dist', appName)));
+
+// Pour toutes les autres requêtes, renvoyer vers le fichier index.html de l'application Angular
+// C'est nécessaire pour que le routage côté client d'Angular fonctionne.
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', appName, 'index.html'));
+});
+
+// Démarrer le serveur
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+}); 
