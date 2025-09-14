25294c3c4719985c9587ef8e55c1f78fc2ef68c9
elouafim <mohammedelouafi02@icloud.com>
2025-09-14 20:10:39 +0100

    dkd
    kdk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pemhjaWxtaG51Ymp5d3F4d2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwOTE1ODksImV4cCI6MjA2NzY2NzU4OX0.kQBraR0AT5ROfETYXFyGtqqAk9YWm0cF4Gqp4IyKTGk'
   },
   apiUrl: 'https://sosmongarage-production-d435.up.railway.app/V1/api',
-  apiUrlLocale: 'https://prolific-quietude-production.up.railway.app/V1/api/'
+  apiUrlLocale: 'http://localhost:8083/V1/api/'
 }; 
