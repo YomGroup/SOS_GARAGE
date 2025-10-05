7ac460ddaa7b8705bd8289da059369718be58636
jesper Elenga <elengajesper@gmail.com>
2025-10-05 18:04:25 +0100

    change of api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     url: 'https://nizhcilmhnubjywqxwjx.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pemhjaWxtaG51Ymp5d3F4d2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwOTE1ODksImV4cCI6MjA2NzY2NzU4OX0.kQBraR0AT5ROfETYXFyGtqqAk9YWm0cF4Gqp4IyKTGk'
   },
-  apiUrl: 'https://sosmongarage-production-d435.up.railway.app/V1/api'
+  apiUrl: 'https://prolific-quietude-production.up.railway.app/V1/api'
 }; 
